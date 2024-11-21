676f1678dd848271ea926d7b6317a471f5be9cfa
ejaysol <111071692+ejaysol@users.noreply.github.com>
2024-11-21 13:34:23 +0800

    Add __pycache__ to .gitignore

--- AMSystem/attendance/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('record_attendance/', views.record_attendance, name='record_attendance'),
-]
--- AMSystem/usermgmt/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('success/', views.success_page, name='success_page'),  # Add this line
-]
